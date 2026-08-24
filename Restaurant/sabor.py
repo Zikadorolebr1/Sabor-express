cfeeb2549f3e4b37107937988d5a15fe6aa978b2
Murillo Rocha <murillorobaina26@gmail.com>
2026-08-24 16:18:17 -0300

    Update 08/24

--- Restaurant/sabor.py
+++ Restaurant/sabor.py
@@ -1,16 +1,18 @@
 from models.restaurante import Restaurante
+from models.cardapio.bebida import Bebida
+from models.cardapio.prato import Prato
 # Código importado utilizando o from e o import
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_refrigerante = Bebida('Pepsi', 'R$7,00', 'Grande')
+prato_macarrao = Prato('Macarronada', 'R$15,00', 'A melhor macarronada do Brasa!')
 # restaurante_japones = Restaurante('Japanzil', 'Japonesa')
 # restaurante_mexicano = Restaurante('MexicanZil', 'Mexicana')
 
-restaurante_praca.receber_avaliacao('Murillo', '10')
-restaurante_praca.receber_avaliacao('Ana', '8.7')
-
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_refrigerante)
+    print(prato_macarrao)
 
 
 if __name__ == '__main__':
