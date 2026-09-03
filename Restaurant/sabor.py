019030d0cd6293e4649f4c89e0be573e582b46d5
Murillo Rocha <murillorobaina26@gmail.com>
2026-09-03 15:40:52 -0300

    Add menu display for restaurant

--- Restaurant/sabor.py
+++ Restaurant/sabor.py
@@ -6,13 +6,15 @@ from models.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_refrigerante = Bebida('Pepsi', 'R$7,00', 'Grande')
 prato_macarrao = Prato('Macarronada', 'R$15,00', 'A melhor macarronada do Brasa!')
+
+restaurante_praca.adicionar_no_cardapio(bebida_refrigerante)
+restaurante_praca.adicionar_no_cardapio(prato_macarrao)
 # restaurante_japones = Restaurante('Japanzil', 'Japonesa')
 # restaurante_mexicano = Restaurante('MexicanZil', 'Mexicana')
 
 
 def main():
-    print(bebida_refrigerante)
-    print(prato_macarrao)
+    restaurante_praca.exibir_cardapio
 
 
 if __name__ == '__main__':
